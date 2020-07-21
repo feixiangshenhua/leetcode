ead86d3d6172c60caea58d2d9e0df893ca3f272b
xiaoyun <391720181@qq.com>
2020-07-21 20:22:15 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         int a = 3;
         Integer b = 3;
+        System.out.println("aaaa");
         System.out.println(a == b);
     }
 }
